4361c511e9fe58980686c98bb9d5d3e25c4e48c8
dyl10s <dylanstrohschein@gmail.com>
2018-03-13 07:30:22 -0400

    Fixed ChessPiece.java from professor.

--- src/game/ChessPiece.java
+++ src/game/ChessPiece.java
@@ -1,39 +1,40 @@
-package game;
 
-public abstract class ChessPiece implements IChessPiece {
 
-	private Player owner;
+public abstract class ChessPiece implements IChessPiece
+{
+   
+    private Player owner;
 
-	/**
-	 * Constructor for objects of class ChessPiece
-	 */
-	public ChessPiece(Player player) {
-		// initialise instance variables
-		this.owner = player;
-	}
+    /**
+     * Constructor for objects of class ChessPiece
+     */
+    public ChessPiece(Player player)
+    {
+        // initialise instance variables
+        this.owner = player;
+    }
 
-	public abstract String type();
-
-	public Player player() {
-		return owner;
-	}
-
-	public boolean isValidMove(Move move, IChessPiece[][] board) {
-		if (move.fromRow == move.toRow && move.fromColumn == move.toColumn) {
-			return false;
-		}
-
-		if (this != board[move.fromRow][move.fromColumn]) {
-			return false;
-		}
-
-		if (board[move.toRow][move.toColumn] != null){
-			if (board[move.toRow][move.toColumn].player() == owner) {
-				return false;
-			}
-		}
-
-		return true;
-
-	}
+    public abstract String type();
+    
+    public Player player() {
+        return owner;
+    }
+    
+    public boolean isValidMove(Move move,IChessPiece[][] board) {
+       if (move.fromRow == move.toRow && move.fromColumn == move.toColumn) {
+           return false;
+        }
+        
+        if ( this != board[move.fromRow][move.fromColumn] ) {
+            return false;
+        }
+        
+        if ( board[move.toRow][move.toColumn] != null &&
+             board[move.toRow][move.toColumn].player() == owner ) {
+            return false;
+        }
+        
+        return true;
+        
+    }
 }
