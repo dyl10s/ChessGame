80cc4767a79f8c1ea926b56168ba2ff8ba626aa2
Josef Hartsough <36377458+JosefHartsough@users.noreply.github.com>
2018-03-27 09:33:31 -0400

    Added comments

--- src/game/Move.java
+++ src/game/Move.java
@@ -1,12 +1,17 @@
+//Class that contains all 4 movements on the board//
 package game;
 
 public class Move {
+	/*fromRow-The row to move from.
+	*fromColumn-The column to move from.
+	*toRow-The row to move to.
+	*toColumn-The column to move to. */
 	
 	public int fromRow, fromColumn, toRow, toColumn;
 
 	public Move() {
 	}
-
+	//Constructor//
 	public Move(int fromRow, int fromColumn, int toRow, int toColumn) {
 		this.fromRow = fromRow;
 		this.fromColumn = fromColumn;
